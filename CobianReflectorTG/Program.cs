638ba23f6c51b7801aa551860ea4ecf56cab72c0
Eugene Gaydarzhi <107859162+e-gaydarzhi-2077@users.noreply.github.com>
2023-04-12 10:29:24 +0300

    Fix all bugs

--- CobianReflectorTG/Program.cs
+++ CobianReflectorTG/Program.cs
@@ -2,82 +2,137 @@
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace CobianReflectorTG
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            //Vars
-            string DirExe = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            string DateNow = DateTime.Now.ToString("yyyy-MM-dd");
-            string PathLog = "C:\\Program Files\\Cobian Reflector\\Logs\\";
-            string PathLogTemp = "C:\\Program Files\\Cobian Reflector\\Logs\\temp\\";
-            string BotID = File.ReadLines(Path.GetDirectoryName(DirExe) + "\\CobianReflectorTG.cfg").ElementAtOrDefault(0);
-            string BotChatID = File.ReadLines(Path.GetDirectoryName(DirExe) + "\\CobianReflectorTG.cfg").ElementAtOrDefault(1);
+            //Check Settings
+            BotSettings();
+        }
 
-            //Create Temp if not exist
-            Directory.CreateDirectory(PathLogTemp);
-
-            if (File.Exists(PathLog + "Cobian Reflector " + DateNow + ".txt"))
+        public static void BotSettings()
+        {
+            if (File.Exists("BotSettings.txt"))
             {
-                Console.WriteLine("File exists");
-
-                //Copy Opened File
-                File.Copy(PathLog + "Cobian Reflector " + DateNow + ".txt", PathLogTemp + "Cobian Reflector " + DateNow + ".txt", true);
-
-                //Get Public IP
-                string externalIpString = new WebClient().DownloadString("http://icanhazip.com").Replace("\\r\\n", "").Replace("\\n", "").Trim();
-                var externalIp = IPAddress.Parse(externalIpString);
-
-                //Read File
-                var lines = File.ReadLines(PathLogTemp + "Cobian Reflector " + DateNow + ".txt");
-                string result = string.Join("\n", lines.Where(s => s.IndexOf("ERR", StringComparison.InvariantCultureIgnoreCase) >= 0));
-                Console.WriteLine(result);
-
-                try
-                {
-                    //Check results if empty
-                    if (result == "")
-                    {
-                        Console.WriteLine("Log file not have errors");
-                    }
-                    else
-                    {
-                        //Check results if have upper characters contains
-                        if (!result.Contains("ERR"))
-                        {
-                            Console.WriteLine("ERR: " + result.Contains("ERR") + "!");
-                        }
-                        else
-                        {
-                            //Send result in Telegram
-                            string GetBot = "Error Cobian Reflector ⚠️ " + "\n" + "User: " + Environment.UserName + "\n" + "Computer: " + Environment.MachineName + "\n" + "IP-Adress: " + externalIp.ToString() + "\n" + "`" + result + "`";
-                            WebRequest reqGET = WebRequest.Create(@"https://api.telegram.org/bot" + BotID + "/sendMessage?chat_id=" + BotChatID + "&parse_mode=Markdown&text=" + GetBot);
-                            WebResponse resp = reqGET.GetResponse();
-                            Stream stream = resp.GetResponseStream();
-                            StreamReader sr = new StreamReader(stream);
-                            Console.WriteLine("Errors not finded");
-                        }
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("File cannot be opened");
-                }
-                finally
-                {
-                    //Delete temp file and exit
-                    File.Delete(PathLogTemp + "Cobian Reflector " + DateNow + ".txt");
-                    Directory.GetFiles(PathLogTemp).ToList().ForEach(File.Delete);
-                    Environment.Exit(0);
-                }
+                ErrCheck();
             }
             else
             {
-                Console.WriteLine("File not exists");
-                Environment.Exit(0);
+                Console.WriteLine("Enter api key for telegram bot:");
+                String StringApi = Console.ReadLine();
+                Console.WriteLine("Enter chat id for telegram bot");
+                String StringId = Console.ReadLine();
+
+                SettingsBot Write = new SettingsBot();
+                Write.CryptBot(StringApi, StringId);
+            }
+        }
+
+        public static void ErrCheck()
+        {
+            //String path
+            string LogFile = "C:\\Program Files\\Cobian Reflector\\Logs\\" + "Cobian Reflector " + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            string LogFileTemp = "C:\\Program Files\\Cobian Reflector\\Logs\\temp\\" + "Cobian Reflector " + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            string PathLogTemp = "C:\\Program Files\\Cobian Reflector\\Logs\\temp\\";
+
+            //Create temp directory if not exist
+            Directory.CreateDirectory(PathLogTemp);
+
+            //If log file exists, copy file and find errors
+            if (File.Exists(LogFile))
+            {
+                //Copy opened file to temp directory
+                File.Copy(LogFile, LogFileTemp, true);
+
+                //Read file and find errors
+                string fileContent = File.ReadAllText(LogFileTemp);
+
+                string errorLine = Regex.Match(fileContent, @"\*\* Number of errors: (\d+)\.").Value;
+                int errorCount;
+                if (int.TryParse(Regex.Match(errorLine, @"\d+").Value, out errorCount))
+                {
+                    if (errorCount > 0)
+                    {
+                        Console.WriteLine($"Error found: {errorCount}");
+
+                        //Find "ERR" lines
+                        string[] errorLines = Regex.Matches(fileContent, @"ERR.*", RegexOptions.Multiline).Cast<Match>().Select(match => match.Value).ToArray();
+                        Console.WriteLine($"Error details: {errorLines.Length}");
+
+                        //Add to builder
+                        StringBuilder sb = new StringBuilder();
+                        foreach (string line in errorLines)
+                        {
+                            sb.AppendLine(line);
+                        }
+                        string errorDetails = sb.ToString();
+                        Console.WriteLine(errorDetails);
+
+                        //Call Telegram
+                        var task = TelegramBot(errorCount.ToString(), errorDetails.ToString());
+                        task.Wait();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No Error found: {errorCount}");
+                    }
+                }
+                else
+
+                {
+                    Console.WriteLine("Number of errors not found");
+                }
+                //Delete temp file and exit
+                Directory.GetFiles(PathLogTemp).ToList().ForEach(File.Delete);
+
+            }
+        }
+
+        static async Task TelegramBot(string ErrorCount, string ErrorDetails)
+        {
+            SettingsBot decryptBot = new SettingsBot();
+            decryptBot.Decrypt();
+
+            string string1 = decryptBot.DecryptedString1;
+            string string2 = decryptBot.DecryptedString2;
+
+
+            string externalIp = GetExternalIp();
+            string VarData = "Cobian Reflector Error⚠️" + "\n" + "User: " + Environment.UserName + "\n" + "Computer: " + Environment.MachineName + "\n" + "Time: " + DateTime.Now + "\n" + "Ip-Adress: " + externalIp + "\n" + hdd() + "\n" + "Error found: " + ErrorCount + "\n" + "`" + ErrorDetails + "`";
+            var client = new System.Net.Http.HttpClient();
+            var url = $"https://api.telegram.org/bot{string1}/sendMessage?chat_id={string2}&parse_mode=Markdown&text={VarData}";
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Bot Ok");
+        }
+
+        public static string GetExternalIp()
+        {
+            using (var client = new WebClient())
+            {
+                string response = client.DownloadString("https://api.ipify.org");
+                return response;
+            }
+        }
+
+        static string hdd()
+        {
+            try
+            {
+                DriveInfo driveInfo = new DriveInfo("C:\\");
+                long freeSpaceInBytes = driveInfo.TotalFreeSpace;
+                double freeSpaceInGB = (double)freeSpaceInBytes / (1024 * 1024 * 1024);
+                return $"Free space on C:\\ drive: {freeSpaceInGB:F2} GB";
+            }
+            catch
+            {
+                return "Error";
             }
         }
     }
---
+++ CobianReflectorTG/SettingsBot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CobianReflectorTG
+{
+    class SettingsBot
+    {
+        public string DecryptedString1 { get; private set; }
+        public string DecryptedString2 { get; private set; }
+
+        public void CryptBot(string StringApi, string StringId)
+        {
+            string encryptedString1 = EncryptString(Environment.MachineName.ToString(), StringApi);
+            string encryptedString2 = EncryptString(Environment.MachineName.ToString(), StringId);
+            using (StreamWriter writer = new StreamWriter("BotSettings.txt"))
+            {
+                writer.WriteLine(encryptedString1);
+                writer.WriteLine(encryptedString2);
+            }
+            Console.WriteLine("Data saved successfully");
+        }
+
+        static string EncryptString(string key, string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] encryptedBytes = new byte[plainTextBytes.Length];
+            for (int i = 0; i < plainTextBytes.Length; i++)
+            {
+                encryptedBytes[i] = (byte)(plainTextBytes[i] ^ keyBytes[i % keyBytes.Length]);
+            }
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public void Decrypt()
+        {
+            string[] encryptedStrings = File.ReadAllLines("BotSettings.txt");
+            DecryptedString1 = DecryptString(Environment.MachineName.ToString(), encryptedStrings[0]);
+            DecryptedString2 = DecryptString(Environment.MachineName.ToString(), encryptedStrings[1]);
+        }
+
+        private string DecryptString(string key, string encryptedText)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] decryptedBytes = new byte[encryptedBytes.Length];
+            for (int i = 0; i < encryptedBytes.Length; i++)
+            {
+                decryptedBytes[i] = (byte)(encryptedBytes[i] ^ keyBytes[i % keyBytes.Length]);
+            }
+            return Encoding.UTF8.GetString(decryptedBytes);
+        }
+
+    }
+}
+
+
